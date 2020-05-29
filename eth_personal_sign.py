83b7a573eb42c2b815fa5cae4aff8a0b148acfe2
zhutianchi <zhutianchi@mcarlo.com>
2020-05-29 18:44:31 +0800

    make the interfaces exact the same

--- eth_personal_sign.py
+++ eth_personal_sign.py
@@ -15,13 +15,24 @@ def keccak256(prehash: bytes) -> bytes:
 
 class EthPersonalSign(object):
     def __init__(self, private_key):
-        self.private_key_to_account(private_key)
+        self.privateKeyToAccount(private_key)
 
-    def private_key_to_account(self, private_key):
-        private_key_bytes = EthPersonalSign.decode_hex(private_key)
+    def privateKeyToAccount(self, private_key):
+        private_key_bytes = decode_hex(private_key)
         self._raw_key = private_key_bytes
 
-    def sign_message_hash(self, msg_hash_bytes: bytes):
+    # def get_public_key(self):
+    #     pass
+
+    # def get_address(self):
+    #     key.public_key.to_checksum_address()
+    #     pass
+
+    # def private_key_to_account(self):
+    #     key = self._parsePrivateKey(private_key)
+    #     return LocalAccount(key, self)
+
+    def signHash(self, msg_hash_bytes: bytes):
         if len(msg_hash_bytes) != 32:
             raise ValueError("The message hash must be exactly 32-bytes")
         key = self._raw_key
@@ -37,74 +48,74 @@ class EthPersonalSign(object):
         v = signature_bytes[64:65]
 
         v = ord(v)
-        v = EthPersonalSign.to_eth_v(v)
+        v = to_eth_v(v)
         v = bytes([v])
 
         eth_signature_bytes = r + s + v
-        return eth_signature_bytes
+        return {
+            'messageHash': msg_hash_bytes,
+            'signature': HexBytes(eth_signature_bytes)
+        }
 
-    @staticmethod
-    def is_0x_prefixed(value) -> bool:
-        return value.startswith("0x") or value.startswith("0X")
+def to_bytes(primitive=None, hexstr: str = None, text: str = None) -> bytes:
+    if isinstance(primitive, bytearray):
+        return bytes(primitive)
+    elif isinstance(primitive, bytes):
+        return primitive
+    elif hexstr is not None:
+        if len(hexstr) % 2:
+            # type check ignored here because casting an Optional arg to str is not possible
+            hexstr = "0x0" + remove_0x_prefix(hexstr)  # type: ignore
+        return decode_hex(hexstr)
+    elif text is not None:
+        return text.encode("utf-8")
+    raise TypeError(
+        "expected a bool, int, byte or bytearray in first arg, or keyword of hexstr or text"
+    )
 
-    @staticmethod
-    def remove_0x_prefix(value: str) -> str:
-        if EthPersonalSign.is_0x_prefixed(value):
-            return value[2:]
-        return value
+def to_eth_v(v_raw, chain_id=None):
+    if chain_id is None:
+        v = v_raw + V_OFFSET
+    else:
+        v = v_raw + CHAIN_ID_OFFSET + 2 * chain_id
+    return v
 
-    @staticmethod
-    def decode_hex(value: str) -> bytes:
-        return codecs.decode(EthPersonalSign.remove_0x_prefix(value), "hex")  # type: ignore
+def is_0x_prefixed(value) -> bool:
+    return value.startswith("0x") or value.startswith("0X")
 
-    @staticmethod
-    def hex_bytes(b: bytes) -> str:
-        return '0x' + b.hex()
+def remove_0x_prefix(value: str) -> str:
+    if is_0x_prefixed(value):
+        return value[2:]
+    return value
 
-    @staticmethod
-    def to_bytes(primitive=None, hexstr: str = None, text: str = None) -> bytes:
-        if isinstance(primitive, bytearray):
-            return bytes(primitive)
-        elif isinstance(primitive, bytes):
-            return primitive
-        elif hexstr is not None:
-            if len(hexstr) % 2:
-                # type check ignored here because casting an Optional arg to str is not possible
-                hexstr = "0x0" + EthPersonalSign.remove_0x_prefix(hexstr)  # type: ignore
-            return EthPersonalSign.decode_hex(hexstr)
-        elif text is not None:
-            return text.encode("utf-8")
-        raise TypeError(
-            "expected a bool, int, byte or bytearray in first arg, or keyword of hexstr or text"
-        )
+def decode_hex(value: str) -> bytes:
+    return codecs.decode(remove_0x_prefix(value), "hex")  # type: ignore
 
-    @staticmethod
-    def signature_wrapper(message, version=b'E'):
-        # watch here for updates to signature format: https://github.com/ethereum/EIPs/issues/191
-        assert isinstance(message, bytes)
-        if version == b'E':
-            preamble = b'\x19Ethereum Signed Message:\n'
-            size = str(len(message)).encode('utf-8')
-            return preamble + size + message
-        else:
-            raise NotImplementedError("Only the 'Ethereum Signed Message' preamble is supported")
+def signature_wrapper(message, version=b'E'):
+    # watch here for updates to signature format: https://github.com/ethereum/EIPs/issues/191
+    assert isinstance(message, bytes)
+    if version == b'E':
+        preamble = b'\x19Ethereum Signed Message:\n'
+        size = str(len(message)).encode('utf-8')
+        return preamble + size + message
+    else:
+        raise NotImplementedError("Only the 'Ethereum Signed Message' preamble is supported")
 
-    @staticmethod
-    def to_eth_v(v_raw, chain_id=None):
-        if chain_id is None:
-            v = v_raw + V_OFFSET
-        else:
-            v = v_raw + CHAIN_ID_OFFSET + 2 * chain_id
-        return v
-        
-    @staticmethod
-    def defunct_hash_message(primitive=None, hexstr=None, text=None):
-        message_bytes = EthPersonalSign.to_bytes(primitive, hexstr=hexstr, text=text)
-        wrapped = EthPersonalSign.signature_wrapper(message_bytes)
-        hashed = keccak256(wrapped)
-        return hashed
+def defunct_hash_message(primitive=None, hexstr=None, text=None):
+    message_bytes = to_bytes(primitive, hexstr=hexstr, text=text)
+    wrapped = signature_wrapper(message_bytes)
+    hashed = keccak256(wrapped)
+    return hashed
 
-    def sign_hash(self, text=None, hexstr=None):
-        msg_hash = EthPersonalSign.defunct_hash_message(hexstr=hexstr, text=text)
-        signature = self.sign_message_hash(msg_hash)
-        return EthPersonalSign.hex_bytes(signature)
+class HexBytes(bytes):
+    def __new__(cls, val: bytes):
+        return super().__new__(cls, val)
+
+    def hex(self):
+        '''
+        Just like :meth:`bytes.hex`, but prepends "0x"
+        '''
+        return '0x' + super().hex()
+
+    def __repr__(self):
+        return 'HexBytes(%r)' % self.hex()
